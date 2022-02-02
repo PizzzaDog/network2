de2438df12561ef6d6372467f9b2ccf757d00890
PizzzaDog <7883494@gmail.com>
2022-02-02 22:44:47 +0300

    Initial commit

---
+++ src/main/java/by/overone/controller/TimeController.java
@@ -0,0 +1,14 @@
+package by.overone.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class TimeController {
+
+
+    @GetMapping("/time")
+    public String getCurrentTime() {
+        return null;
+    }
+}
