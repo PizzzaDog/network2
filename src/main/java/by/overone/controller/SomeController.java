2a463bebcc0ecaeb04b567d8db8f11d366cd8e0c
PizzzaDog <7883494@gmail.com>
2022-10-04 21:19:50 +0400

    added delete method in SomeController

--- src/main/java/by/overone/controller/SomeController.java
+++ src/main/java/by/overone/controller/SomeController.java
@@ -1,9 +1,10 @@
 package by.overone.controller;
 
-
 import by.overone.entity.User;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
 
 @RestController
 public class SomeController {
@@ -11,5 +12,10 @@ public class SomeController {
     @GetMapping("/some")
     public User getSomeUser() {
         return new User();
+    }
+
+    @DeleteMapping("/some")
+    public void deleteSomeUser() {
+        //УДАЛЕНИЕ РАНДОМНОГО ЮЗЕРА
     }
 }
