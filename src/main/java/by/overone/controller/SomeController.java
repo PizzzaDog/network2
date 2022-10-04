00a3ff2a2246a78c3bec9f5838040eef5088e0c1
PizzzaDog <7883494@gmail.com>
2022-10-04 20:54:44 +0400

    added SomeController and endpoints

---
+++ src/main/java/by/overone/controller/SomeController.java
@@ -0,0 +1,15 @@
+package by.overone.controller;
+
+
+import by.overone.entity.User;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class SomeController {
+
+    @GetMapping("/some")
+    public User getSomeUser() {
+        return new User();
+    }
+}
